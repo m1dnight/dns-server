c4f0e682e5234726ab4d9778308cc53af4159313
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-06-26 18:05:20 +0200

    wip commit

---
+++ src/ServerTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace codecrafters_dns_server;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
